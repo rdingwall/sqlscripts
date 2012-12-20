5b8fc4e585797b0794b8c4ae3640e39ee3c31ec2
Richard Dingwall <rdingwall@gmail.com>
2012-12-20 10:59:09 +0000

    Oracle drop_all_objects script now ignores MLOG$ and RUPD$ tables, PURGEs tables as it goes to recover space, and outputs to the console which objects it is dropping.

--- oracle/drop_all_objects_in_schema.sql
+++ oracle/drop_all_objects_in_schema.sql
@@ -3,25 +3,35 @@
 -- and recreate the entire user. 
 
 -- This script will destroy all objects in the schema!! Use with caution!!
+whenever sqlerror exit sql.sqlcode rollback;
+set serveroutput on;
 declare
-cursor c_get_objects is
-  select object_type,'"'||object_name||'"'||decode(object_type,'TABLE' ,' cascade constraints',null) obj_name
-  from user_objects
-  where object_type in ('TABLE','VIEW','PACKAGE','SEQUENCE','SYNONYM', 'MATERIALIZED VIEW')
-  order by object_type;
-  
-cursor c_get_types is
-  select object_type, '"'||object_name||'"' obj_name
-  from user_objects
-  where object_type in ('TYPE');
+  cursor c_get_objects is
+    select object_type,'"'||object_name||'"'||decode(object_type,'TABLE' ,' cascade constraints purge',null) obj_name
+    from user_objects
+    where object_type in ('TABLE','VIEW','PACKAGE','SEQUENCE','SYNONYM', 'MATERIALIZED VIEW')
+    and object_name not like 'MLOG$%' -- materialized log view tables (will be dropped automatically)
+    and object_name not like 'RUPD$%' -- ignore updateable snapshots
+    order by object_type;
+    
+  cursor c_get_types is
+    select object_type, '"'||object_name||'"' obj_name
+    from user_objects
+    where object_type in ('TYPE');
+    
+  v_sql varchar2(255);
   
 begin
   for object_rec in c_get_objects loop
-    execute immediate ('drop '||object_rec.object_type||' ' ||object_rec.obj_name);
+    v_sql := 'drop '||object_rec.object_type||' ' ||object_rec.obj_name;
+    dbms_output.put_line(v_sql);
+    execute immediate (v_sql);
   end loop;
   for object_rec in c_get_types loop
   begin
-    execute immediate ('drop '||object_rec.object_type||' ' ||object_rec.obj_name);
+    v_sql := 'drop '||object_rec.object_type||' ' ||object_rec.obj_name;
+    dbms_output.put_line(v_sql);
+    execute immediate (v_sql);
   end;
   end loop;
 end;
